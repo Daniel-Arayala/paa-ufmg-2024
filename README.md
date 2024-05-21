6164401249e5644a9f620f1b3b269c2d90e9791d
Daniel Ayala <idd_dayala@uolinc.com>
2024-05-21 00:16:56 -0300

    feat(problem5): Problem of secret chamber at mount rushmore working with dfs implementation but time limit exceeded

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # paa-ufmg-2024
 Repositório contendo o código fonte para a realização dos TPs
+
+
+# Create a table for t
+
+
+| #    | ID    |      PROBLEMA                       | SUBMISSÃO | ACEITO | NÍVEL | PESO |
+|------|-------|-------------------------------------|-----------|--------|-------|------|
+| 1    | 1085  |  Babel                              | -         | -      | 8     | 100  |
+| 2    | 1205  |  Cerco a Leningrado                 | -         | -      | 7     | 100  |
+| 3    | 1447  |  Back to the Future                 | -         | -      | 7     | 100  |
+| 4    | 1552  |  Resgate em Queda Livre             | -         | -      | 6     | 100  |
+| 5    | 2646  |  Secret Chamber at Mount Rushmore   | -         | -      | 4     | 100  |
+| 6    | 2962  |  Arte Valiosa                       | -         | -      | 9     | 100  |
---
+++ TPs/TP1/problem5/secret-chamber-at-mount-rushmore.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+
+# define YES "yes"
+# define NO "no"
+
+using namespace std;
+
+class TranslationTree {
+    private:
+        unordered_map<char, unordered_set<char>> ttree;
+    public:
+        TranslationTree() : ttree({}) {}
+        void addTranslation(char srcLetter, char destLetter) {
+            ttree[srcLetter].insert(destLetter);
+        }
+
+        bool letterExistsInTree(char letter) {
+            return ttree.find(letter) != ttree.end();
+        }
+
+        // Implementation of DFS
+        bool isTranslation(const char& srcLetter, const char& destLetter, int counter = 0) {
+            if (
+                (srcLetter == destLetter)
+                || (ttree[srcLetter].find(destLetter) != ttree[srcLetter].end())) { // Quick search to find the destLetter in the children
+                return true;
+            }
+            else if (
+                (!letterExistsInTree(srcLetter)) 
+                 || (counter > ttree.size())) { // To avoid infinite loops in case of cycles
+                return false;
+            }
+            else             
+            {
+                bool foundTranslation = false;
+                for (char nextLetter : ttree[srcLetter]) {
+                    foundTranslation = isTranslation(nextLetter, destLetter, counter + 1);
+                    if (foundTranslation) {
+                        foundTranslation = true;
+                        break;
+                    }
+                }
+
+                return foundTranslation;
+            }
+        }
+        
+};
+
+void createTranslations(
+    const int& numTranslations, 
+    unique_ptr<TranslationTree>& translationTree){
+
+    for(int i = 0; i < numTranslations; i++){
+        char srcLetter, destLetter;
+        cin >> srcLetter >> destLetter;
+        translationTree->addTranslation(srcLetter, destLetter);
+    }
+}
+
+void matchWordPairs(
+    const int& numWordPairs,
+    unique_ptr<TranslationTree>& translationTree){
+    
+    for (int i = 0; i < numWordPairs; i++){
+        string baseWord, translatedWord;
+        bool isTranslation = true;
+        cin >> baseWord >> translatedWord;
+
+        if (baseWord.length() == translatedWord.length()) {
+            for (int j = 0; j < baseWord.length(); j++) {
+                if (!translationTree->isTranslation(baseWord[j], translatedWord[j])) {
+                    isTranslation = false;
+                    break;
+                }
+            }
+        }
+        else {
+            isTranslation = false;
+        }
+        
+        // Writes to standard output
+        cout << (isTranslation ? YES : NO) << endl;
+
+    }
+    
+}
+
+
+
+int main() {
+    // VARIABLES READ FROM INPUT
+    int numTranslations = 0;
+    int numWordPairs = 0;
+    unique_ptr<TranslationTree> translationTree(new TranslationTree());
+    
+    // Reads the total number of translations and word pairs
+    cin >> numTranslations >> numWordPairs;
+
+    createTranslations(numTranslations, translationTree);
+
+    matchWordPairs(numWordPairs, translationTree);
+
+    return 0;     
+}
